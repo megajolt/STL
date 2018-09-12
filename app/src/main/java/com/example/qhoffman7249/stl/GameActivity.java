9e7e225b8ba6088655d057af4c30ce19f52c8316
Quinn  Hoffman <qhh746@gmail.com>
2018-09-12 09:01:48 -0700

    Added activity for Main game, and java files for each of the rooms

---
+++ app/src/main/java/com/example/qhoffman7249/stl/GameActivity.java
@@ -0,0 +1,13 @@
+package com.example.qhoffman7249.stl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
