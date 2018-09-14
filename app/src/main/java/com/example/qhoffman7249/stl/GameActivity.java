c6f92f3ea60f9bc5aa6e22a35ff8ee7194adaaa4
Quinn  Hoffman <qhh746@gmail.com>
2018-09-14 10:02:17 -0700

    Stuff

--- app/src/main/java/com/example/qhoffman7249/stl/GameActivity.java
+++ app/src/main/java/com/example/qhoffman7249/stl/GameActivity.java
@@ -1,16 +1,46 @@
 package com.example.qhoffman7249.stl;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
 
 public class GameActivity extends AppCompatActivity {
-    int health=100;
+    public ProgressBar healthBar;
+    public int currentHealth=100;
+    public Handler healthHandler= new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+
+        healthBar=(ProgressBar) findViewById(R.id.progressbar);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(currentHealth>0){
+                    currentHealth=currentHealth-1;
+                    android.os.SystemClock.sleep(50);
+                    healthHandler.post(new Runnable(){
+                        @Override
+                      public void run(){
+                            healthBar.setProgress(currentHealth);
+                        }
+                    });
+                }
+               healthHandler.post(new Runnable () {
+                   @Override
+                   public void run() {
+
+                   }
+
+               });
+            }
+        }).start();
     }
 
 
