0a21f9224379fc44cd35c97c4876e25fba5fdfb4
Quinn  Hoffman <qhh746@gmail.com>
2018-09-24 09:44:19 -0700

    Green person

--- app/src/main/java/com/example/qhoffman7249/stl/Shields.java
+++ app/src/main/java/com/example/qhoffman7249/stl/Shields.java
@@ -1,22 +1,25 @@
 package com.example.qhoffman7249.stl;
 
 public class Shields {
+
     int shieldHealth=100;
-    int damage=5; //placeholder
+     //placeholder
     //when damaged decrease shield health, if there is excess damage after shield is destroyed damage ship health
-    public void OnDamage(/*damage*/){
+    public int OnDamage(int damage){
         if(shieldHealth>0) {
             if (damage <= shieldHealth) {
                 shieldHealth = shieldHealth - damage;
             }
             else if(damage>shieldHealth){
-                shieldHealth=0;
                 damage=damage-shieldHealth;
+
                 // call health subtraction method with damage
             }
         }
         else{
             // call health subtraction method with damage
         }
+       return shieldHealth;
     }
+
 }
