5c322aeb965f9e0ff5da88c8a63116b8020032a2
emedley9519 <yeldemsaile@gmail.com>
2018-10-02 10:00:06 -0700

    weapons testing

--- app/src/main/java/com/example/qhoffman7249/stl/Gun.java
+++ app/src/main/java/com/example/qhoffman7249/stl/Gun.java
@@ -1,4 +1,9 @@
 package com.example.qhoffman7249.stl;
 
 public class Gun {
+    public int bastardSword=5;
+    public int maul=35;
+    public int glaive=150;
+    public int pruningShears=2;
+    public int halberd=65;
 }
