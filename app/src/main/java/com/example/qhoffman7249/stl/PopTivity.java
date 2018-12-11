5b56893eea53e3eafc9e1a3d5baec5c429b619d4
team3636 <ghsrobotics3636@gmail.com>
2018-12-11 01:57:58 -0800

    fixed the menu a little bit now all of the buttons are visible they still don't do anything though, planning on useing Intent.putExtras();

--- app/src/main/java/com/example/qhoffman7249/stl/PopTivity.java
+++ app/src/main/java/com/example/qhoffman7249/stl/PopTivity.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
+import android.widget.Toast;
 
 public class PopTivity extends AppCompatActivity {
-
+    public ScrollView current;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +25,8 @@ public class PopTivity extends AppCompatActivity {
         height = height * .8;
         width = width * .6;
 
+        current = findViewById(R.id.weapons);
+
         int intwidth = (int)width;
         int intheight = (int)height;
         getWindow().setLayout(intwidth,intheight);
@@ -31,20 +35,38 @@ public class PopTivity extends AppCompatActivity {
         showrooms.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ScrollView weapons = findViewById(R.id.weapons);
-                weapons.setVisibility(View.GONE);
-                ScrollView rooms = findViewById(R.id.rooms);
-                rooms.setVisibility(View.VISIBLE);
+                current.setVisibility(View.GONE);
+                current = findViewById(R.id.rooms);
+                current.setVisibility(View.VISIBLE);
             }
         });
         Button showweapons = findViewById(R.id.firstSubM);
         showweapons.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ScrollView rooms = findViewById(R.id.rooms);
-                rooms.setVisibility(View.GONE);
-                ScrollView weapons = findViewById(R.id.weapons);
-                weapons.setVisibility(View.VISIBLE);
+                current.setVisibility(View.GONE);
+                current = findViewById(R.id.weapons);
+                current.setVisibility(View.VISIBLE);
+            }
+        });
+        Button showcrew = findViewById(R.id.thirdSubM);
+        showcrew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                current.setVisibility(View.GONE);
+                current = findViewById(R.id.crew);
+                current.setVisibility(View.VISIBLE);
+            }
+        });
+        Button showpowerreg = findViewById(R.id.fourthSubM);
+        showpowerreg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int name = v.getId();
+                Toast.makeText(PopTivity.this, "name: " + name, Toast.LENGTH_SHORT).show();
+                current.setVisibility(View.GONE);
+                current = findViewById(R.id.power);
+                current.setVisibility(View.VISIBLE);
             }
         });
     }
